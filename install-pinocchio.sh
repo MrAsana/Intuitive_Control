60007ff28535957790a7fcf7d8d3481dc212785e
Tianren Song <str19990211@gmail.com>
2024-02-07 21:54:55 +0800

    fix install

--- install-pinocchio.sh
+++ install-pinocchio.sh
@@ -16,3 +16,9 @@ export PKG_CONFIG_PATH=/opt/openrobots/lib/pkgconfig:$PKG_CONFIG_PATH
 export LD_LIBRARY_PATH=/opt/openrobots/lib:$LD_LIBRARY_PATH
 export PYTHONPATH=/opt/openrobots/lib/python3.10/site-packages:$PYTHONPATH # Adapt your desired python version here
 export CMAKE_PREFIX_PATH=/opt/openrobots:$CMAKE_PREFIX_PATH
+
+echo 'export PATH=/opt/openrobots/bin:$PATH
+export PKG_CONFIG_PATH=/opt/openrobots/lib/pkgconfig:$PKG_CONFIG_PATH
+export LD_LIBRARY_PATH=/opt/openrobots/lib:$LD_LIBRARY_PATH
+export PYTHONPATH=/opt/openrobots/lib/python3.10/site-packages:$PYTHONPATH # Adapt your desired python version here
+export CMAKE_PREFIX_PATH=/opt/openrobots:$CMAKE_PREFIX_PATH' >> ~/.bashrc
--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+3-d#!/bin/bash
 sudo apt update && sudo apt upgrade
-sudo apt-get install  -y  git python3-pip python-is-python3 cmake gcc g++ libopencv-dev build-essential libglib2.0-dev libeigen3-dev net-tools libgl1-mesa-dev libegl1-mesa-dev libglew-dev
+sudo apt-get install  -y  git python3-pip python-is-python3 cmake gcc g++ libopencv-dev build-essential libglib2.0-dev libeigenev net-tools libgl1-mesa-dev libegl1-mesa-dev libglew-dev
 mkdir ~/tools/
 cd ~/tools/
 git clone https://github.com/MrAsana/Intuitive_Control.git
@@ -29,9 +29,11 @@ sudo make install
 
 echo 'export LD_LIBRARY_PATH=~/tools/Pangolin/build/src/:~/tools/ORB_SLAM3/Thirdparty/DBoW2/lib:~/tools/ORB_SLAM3/Thirdparty/g2o/lib:~/tools/ORB_SLAM3/lib:$LD_LIBRARY_PATH' >> ~/.bashrc
 
-bash ~/tools/install-pinocchio.sh
-
-
 cd ~/tools/Intuitive_Control/vio_arm
 pip install -r requirements.txt
+
+
+
+bash ~/tools/install-pinocchio.sh
+
 
