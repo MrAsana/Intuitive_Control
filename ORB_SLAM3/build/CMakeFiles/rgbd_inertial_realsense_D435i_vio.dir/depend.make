ef30c9261fccbfa4c080f64dda3b7847413f774f
Tianren Song <str19990211@gmail.com>
2024-02-07 19:59:30 +0800

    fix install

---
+++ ORB_SLAM3/build/CMakeFiles/rgbd_inertial_realsense_D435i_vio.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for rgbd_inertial_realsense_D435i_vio.
+# This may be replaced when dependencies are built.
---
+++ ORB_SLAM3/build/CMakeFiles/rgbd_realsense_D435i_vio.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../RGB-D-VIO/rgbd_realsense_D435i_vio"
+  "../RGB-D-VIO/rgbd_realsense_D435i_vio.pdb"
+  "CMakeFiles/rgbd_realsense_D435i_vio.dir/RGB-D-VIO/rgbd_realsense_D435i.cc.o"
+  "CMakeFiles/rgbd_realsense_D435i_vio.dir/RGB-D-VIO/rgbd_realsense_D435i.cc.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rgbd_realsense_D435i_vio.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
